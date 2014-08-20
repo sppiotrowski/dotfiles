1ae783fe60aace9d532c683003cc56807b6be6ed
Sebastian Piotrowski <sebastian.piotrowski@blstream.com>
2014-08-20 21:05:35 +0200

    Update

--- TODO.md
+++ TODO.md
@@ -1,6 +1,8 @@
 TODO
 * update .spp/bootstrap.sh with repo changes
 * adopt syntastic for bash checking
+* check why eclim-junit does not work
+* add correct permission for bootstrap.sh
 
 DONE
 2014.08.20
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,16 @@
 
 set -u
 
-SPP_HOME=${HOME}/.spp
+SPP_HOME=$HOME/.spp
+FORCE=
+
+function symlink() {
+    if [ -l $2 ] and [ -e $FORCE ]; then
+        echo "rm: $2"
+        rm $2
+    fi
+    ln -s $1 $2
+}
 
 function apt_install() {
     if [ $(dpkg -l $1 | wc -l 2>/dev/null) == "0" ]; then
@@ -18,58 +27,51 @@ function gem_install() {
     fi
 }
 
-function clean() {
-    echo cleaning env
-    if [ $FORCE ]; then
-        echo you are using the force
-        rm -rf $SPP_HOME
-        rm $HOME/.vimrc
-        rm $HOME/.gitconfig
-        rm $HOME/.tmux.conf
-        rm $HOME/.local/share/applications/sqldeveloper.desktop
-        rm $HOME/.local/share/applications/eclipse.desktop
-        rm $HOME/.tmuxinator
-        rm $HOME/.vim
+function init_spp() {
+    echo "exec: $0"
+
+    if [ -d $SPP_HOME ]; then
+        cd $SPP_HOME && git pull
+    else
+        git clone https://github.com/sppiotrowski/dotfiles.git $SPP_HOME
     fi
+
+    symlink ~/dev/spp ~/.spp
+
+    if [ ! -e $SPP_HOME/vim/bundle/vundle ]; then
+        git clone https://github.com/gmarik/vundle.git $SPP_HOME/vim/bundle/vundle
+    fi
+
 }
 
-function make_dirs() {
-    echo making dirs
-
-    if [ -d "$SPP_HOME" ]; then
-        echo Error: ~/.spp already exists
-        exit 1
-    fi
-
-    ln -s ~/dev/spp ~/.spp
-
-    # git clone https://github.com/sppiotrowski/dotfiles.git $SPP_HOME
-    git clone https://github.com/gmarik/vundle.git ${SPP_HOME}/vim/bundle/vundle
-    ln -s $SPP_HOME/unity/eclipse.desktop $HOME/.local/share/applications/eclipse.desktop
-    ln -s $SPP_HOME/unity/sqldeveloper $HOME/.local/share/applications/sqldeveloper.desktop
+function setup_unity() {
+    echo "exec: $0"
+    symlink $SPP_HOME/unity/eclipse.desktop $HOME/.local/share/applications/eclipse.desktop
+    symlink $SPP_HOME/unity/sqldeveloper $HOME/.local/share/applications/sqldeveloper.desktop
 }
 
 function setup_vim() {
-
-    # setup vim env
-    ln -s ${SPP_HOME}/vimrc ${HOME}/.vimrc
-    ln -s ${SPP_HOME}/vim ${HOME}/.vim
+    echo "exec: $0"
+    symlink $SPP_HOME/vimrc $HOME/.vimrc
+    symlink $SPP_HOME/vim $HOME/.vim
 
     # install plugins via vundle
      vim --noplugin -u vim/vundles.vim -N "+set hidden" "+syntax on" +BundleClean! +BundleInstall +qall
 }
 
 function setup_tmux() {
-    apt_install 'tmux'
-    ln -s ${SPP_HOME}/tmux/tmux.conf ${HOME}/.tmux.conf
+    echo "exec: $0"
+    apt_install tmux
+    symlink $SPP_HOME/tmux/tmux.conf $HOME/.tmux.conf
 
     gem_install tmuxinator
-    ln -s $SPP_HOME/tmux/tmuxinator $HOME/.tmuxinator
+    symlink $SPP_HOME/tmux/tmuxinator $HOME/.tmuxinator
 }
 
 function setup_git() {
+    echo "exec: $0"
     apt_install 'git'
-    ln -s ${SPP_HOME}/gitconfig ${HOME}/.gitconfig
+    symlink $SPP_HOME/gitconfig $HOME/.gitconfig
 }
 
 function show_usage() {
@@ -77,29 +79,31 @@ function show_usage() {
 }
 
 function setup_gdrive() {
-    # add-apt-repository ppa:thefanclub/grive-tools
-    # apt-get update
+    echo "exec: $0"
+    add-apt-repository ppa:thefanclub/grive-tools
+    apt-get update
 
     apt_install 'grive-tools'
 }
 
 function bootstrap() {
-    make_dirs
+    [ -n $FORCE ] && rm -rf $SPP_HOME
+    init_spp
+    setup_unity
     setup_vim
     setup_tmux
     setup_git
-    setup_gdrive
+    # setup_gdrive # skipped: time consuming:w
 }
 
 if [ ! $* ]; then
-    FORCE=
-    clean && bootstrap
+    bootstrap
 else
     while getopts hf flag; do
         case $flag in
             f)
                 FORCE=true
-                clean && bootstrap
+                bootstrap
                 ;;
             h)
                 show_usage
